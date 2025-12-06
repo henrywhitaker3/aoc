216118b3c62c5e0ebc65f97db06ccaac57cbf216
Henry Whitaker <henrywhitaker3@outlook.com>
2025-12-06 22:08:35 +0000

    feat: 2025 day 6 part 2
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- internal/twentyfive/day6/run.go
+++ internal/twentyfive/day6/run.go
@@ -9,12 +9,13 @@ import (
 	"fmt"
 	"io"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 var (
-	partMatch = regexp.MustCompile(`([0-9-*.+]+)`)
+	partMatch = regexp.MustCompile(`([0-9]+)|([+*]{1})`)
 )
 
 type Operator string
@@ -112,9 +113,80 @@ func ParseData(data []byte) ([]Calculation, error) {
 	return out, nil
 }
 
+func ParseRTL(data []byte) ([]Calculation, error) {
+	rotated := rotate(data)
+	spl := strings.Split(rotated, "\n")
+
+	unparsed := [][]string{}
+	buffer := []string{}
+	for _, s := range spl {
+		matches := partMatch.FindAllString(s, -1)
+		if len(matches) == 0 {
+			// We've reached the end of the calc
+			unparsed = append(unparsed, slices.Clone(buffer))
+			buffer = []string{}
+			continue
+		}
+		buffer = append(buffer, matches...)
+	}
+	unparsed = append(unparsed, buffer)
+
+	out := []Calculation{}
+	for _, group := range unparsed {
+		calc := Calculation{}
+		for _, m := range group {
+			switch m[0] {
+			case '+':
+				calc.Operator = Add
+				continue
+			case '*':
+				calc.Operator = Multiply
+				continue
+			}
+			dig, err := strconv.Atoi(m)
+			if err != nil {
+				return nil, fmt.Errorf("parse digit: %w", err)
+			}
+			calc.Numbers = append(calc.Numbers, Operand(dig))
+		}
+		slices.Reverse(calc.Numbers)
+		out = append(out, calc)
+	}
+
+	slices.Reverse(out)
+
+	return out, nil
+}
+
+func rotate(data []byte) string {
+	lines := strings.Split(string(data), "\n")
+
+	rows := len(lines)
+	cols := 0
+	for _, line := range lines {
+		cols = max(cols, len(line))
+	}
+
+	rotated := make([]string, cols)
+
+	for i := range rows {
+		line := lines[i]
+		for j := 0; j < cols; j++ {
+			if j < len(line) {
+				rotated[j] += string(line[j])
+			}
+		}
+	}
+
+	return strings.Join(rotated, "\n")
+}
+
 func parseDigits(m []string) ([]Operand, error) {
 	out := []Operand{}
 	for _, m := range m {
+		if m == "" {
+			continue
+		}
 		dig, err := strconv.Atoi(m)
 		if err != nil {
 			return nil, fmt.Errorf("parse int: %w", err)
@@ -127,6 +199,9 @@ func parseDigits(m []string) ([]Operand, error) {
 func parseOperators(m []string) ([]Operator, error) {
 	out := []Operator{}
 	for _, m := range m {
+		if m == "" {
+			continue
+		}
 		var op Operator
 		switch Operator(m) {
 		case Multiply:
@@ -158,5 +233,12 @@ func PartOne(ctx context.Context) error {
 }
 
 func PartTwo(ctx context.Context) error {
+	calcs, err := ParseRTL([]byte(input))
+	if err != nil {
+		return fmt.Errorf("parse input: %w", err)
+	}
+
+	fmt.Printf("Sum: %d\n", SumResults(calcs))
+
 	return nil
 }
--- internal/twentyfive/day6/run_test.go
+++ internal/twentyfive/day6/run_test.go
@@ -34,3 +34,25 @@ func TestItSumsAnswers(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, 4277556, SumResults(calcs))
 }
+
+func TestItSumsRTLAnswers(t *testing.T) {
+	calcs, err := ParseRTL([]byte(sample))
+	require.Nil(t, err)
+	require.Equal(t, 3263827, SumResults(calcs))
+}
+
+func TestItParsesRTLData(t *testing.T) {
+	common.TestLogger(t)
+	calcs, err := ParseRTL([]byte(sample))
+	require.Nil(t, err)
+	require.Equal(
+		t,
+		[]Operand{
+			4,
+			431,
+			623,
+		},
+		calcs[0].Numbers,
+	)
+	require.Equal(t, Add, calcs[0].Operator)
+}
